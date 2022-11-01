cbcacd5b38fc504ac75bd89fa9b73c15a9e6d2fa
artem-liamichev <76532860+artem-liamichev@users.noreply.github.com>
2022-11-01 21:31:40 +0300

    homepage deleted from package.json

--- frontend/src/utils/apiAuth.js
+++ frontend/src/utils/apiAuth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.liamichev.students.nomoredomains.icu';
+export const BASE_URL = 'http://localhost:3000';
 
 const checkServerResponse = (res) => {
   if (res.ok) {
